8062d2d51d0fc33d6dc15e824304c8942fb1f56f
appjedi <54815862+appjedi@users.noreply.github.com>
2022-03-09 05:30:35 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to App Jedi!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to App Jedi
-        </a>
+       
       </header>
     </div>
   );
